1c697c036a7c25ff3562562a10f5215fb9b9d889
DAXILSAXENA <daxildaxil@gmail.com>
2021-06-29 18:35:19 +0530

    Added Feature to Validate Email ID using REGEX

--- src/test/java/com/bridgelabz/UserRegistration/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistration/UserRegistrationTest.java
@@ -2,22 +2,30 @@ package com.bridgelabz.UserRegistration;
 
 import static org.junit.Assert.assertTrue;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class UserRegistrationTest {
 
-	@Test
+	@Before
 	public void checkFirstName() {
 		UserRegistration object = new UserRegistration();
 		String result = object.inputFirstName();
 		assertTrue(result.matches("^[A-Z]{1}[a-z]{2,}$"));
 	}
 
-	@After
+	@Test
 	public void checkLastName() {
 		UserRegistration object = new UserRegistration();
 		String result = object.inputLastName();
 		assertTrue(result.matches("^[A-Z]{1}[a-z]{2,}$"));
+	}
+
+	@After
+	public void checkEmail() {
+		UserRegistration object = new UserRegistration();
+		String result = object.inputEmail();
+		assertTrue(result.matches("[A-Za-z0-9._+%-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,4}"));
 	}
 
 }
